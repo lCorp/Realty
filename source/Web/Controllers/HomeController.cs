2dde9cbe6ec2815998bab1087f45ec1a5073619c
buddyngo <buddyngo@yahoo.com>
2014-03-31 01:34:20 +0700

    Minimize and re-structure solution

---
+++ source/Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Persistence;
+using Core.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context _context = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View(_context.Accounts);
+        }
+
+        public ActionResult Create(Account model)
+        {
+            _context.Accounts.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
